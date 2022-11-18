a2d11dcb4ce20a795f024d1efce5a7f8e6768045
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2022-11-18 15:31:42 +0200

    solved exercises assignment

--- Programming Basics Online Exam - 20 and 21 April 2019/easterLunch.js
+++ Programming Basics Online Exam - 20 and 21 April 2019/easterLunch.js
@@ -1,5 +1,19 @@
+
+
 function easterLunch(input) {
+
+    let easterBreadCount = Number(input[0]);
+    let eggsPackCount = Number(input[1]);
+    let biscuitsKg = Number(input[2]);
+
+    let easterBreadPrice = easterBreadCount * 3.20;
+    let coloringPricePerEggsPack = 12 * 0.15;
+    let coloringEggsPackPrice = eggsPackCount * (coloringPricePerEggsPack + 4.35);
+    let biscuitsPrice = biscuitsKg * 5.40;
+
+    let totalSum = easterBreadPrice + coloringEggsPackPrice + biscuitsPrice;
+     console.log(totalSum.toFixed(2));
 
 }
 
-easterLunch(["3", "2", "3"]);
+easterLunch(["4", "4", "3"]);
