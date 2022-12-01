ba98d36b1c1eb43c11d8a39058af4005ba7e7fc9
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2022-12-01 13:58:07 +0200

    solved exercises assignment

--- While Loop - Lab/graduation.js
+++ While Loop - Lab/graduation.js
@@ -1,6 +1,32 @@
 function solve(input) {
+    let index = 0;
+    let name = input[index];
+    index ++;
+    let year = 1;
+    let examFailed = 0;
+    let excluded = false;
+    let totalSum = 0;
     
-
+    while (year <= 12) {
+        let cuurGrade = Number(input[index]);
+        index ++;
+        if (cuurGrade < 4) {
+            examFailed ++;
+            if (examFailed === 2) {
+                excluded = true;
+                break;
+            }
+            continue;
+        }
+        totalSum += cuurGrade;
+        year ++;
+    }
+    let averageGrade = totalSum / 12;
+    if (excluded === false) {
+        console.log(`${name} graduated. Average grade: ${averageGrade.toFixed(2)}`);
+    } else {
+        console.log(`${name} has been excluded at ${year} grade`);
+    }
 }
 
-solve(["Gosho", "5", "5.5", "6","5.43", "5.5", "6", "5.55", "5", "6", "6", "5.43", "5"]);
+solve(["Mimi", "5", "6", "5","6", "5", "6", "6", "2", "3"]);
