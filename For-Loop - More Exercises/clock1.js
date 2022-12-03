e738dfb202b7e3796c5b669e7ba436794e6e765c
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2022-12-03 20:22:21 +0200

    solved exercises assignment

---
+++ For-Loop - More Exercises/clock1.js
@@ -0,0 +1,11 @@
+function solve() {
+
+    for (let h = 0; h < 24; h ++) {
+        for (let m = 0; m < 60; m ++) {
+            console.log(`${h} : ${m}`);
+        }
+    }
+}
+
+solve();
+//"{час} : {минути}". 
