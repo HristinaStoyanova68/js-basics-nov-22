60bc072b456145a981cce7c1e59a2e928ef5f61b
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2022-12-12 17:45:26 +0200

    solved exercises assignment

---
+++ Programming Basics Online Exam - 6 and 7 April 2019/cinemaTickets.js
@@ -0,0 +1,71 @@
+function solve(input) {
+
+    let index = 0;
+    let nameOfMovie = input[index];
+    index++;
+
+    let studentTicketsCount = 0;
+    let standardTicketsCount = 0;
+    let kidsTicketsCount = 0;
+
+    while (nameOfMovie !== "Finish") {
+        let availableSpaces = Number(input[index]);
+        index++;
+        let typeOfTickets = input[index];
+        index++;
+
+        let spacesTaken = 0;
+
+        while (typeOfTickets !== "End") {
+
+            spacesTaken += 1;
+            if (typeOfTickets === "student") {
+                studentTicketsCount++;
+            } else if (typeOfTickets === "standard") {
+                standardTicketsCount++;
+            } else if (typeOfTickets === "kid") {
+                kidsTicketsCount++;
+            }
+            if (spacesTaken === availableSpaces) {
+                break;
+            }
+            typeOfTickets = input[index];
+            index++;
+        }
+        let percentFull = (spacesTaken / availableSpaces) * 100;
+        console.log(`${nameOfMovie} - ${percentFull.toFixed(2)}% full.`);
+
+        nameOfMovie = input[index];
+        index++;
+    }
+    let totalTickets = standardTicketsCount + studentTicketsCount + kidsTicketsCount;
+    console.log(`Total tickets: ${totalTickets}`);
+    let percentStudentTickets = (studentTicketsCount / totalTickets) * 100;
+    let percentStandardTickets = (standardTicketsCount / totalTickets) * 100;
+    let percentKidsTickets = (kidsTicketsCount / totalTickets) * 100;
+
+    console.log(`${percentStudentTickets.toFixed(2)}% student tickets.`);
+    console.log(`${percentStandardTickets.toFixed(2)}% standard tickets.`);
+    console.log(`${percentKidsTickets.toFixed(2)}% kids tickets.`);
+
+
+}
+
+solve(["Taxi",
+"10",
+"standard",
+"kid",
+"student",
+"student",
+"standard",
+"standard",
+"End",
+"Scary Movie",
+"6",
+"student",
+"student",
+"student",
+"student",
+"student",
+"student",
+"Finish"]);
