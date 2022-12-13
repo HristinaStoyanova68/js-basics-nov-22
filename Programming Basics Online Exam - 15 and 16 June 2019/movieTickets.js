9d6870494d8d45a31e68f2e932e7676d2d43f212
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2022-12-13 17:40:05 +0200

    solved exercises assignment

---
+++ Programming Basics Online Exam - 15 and 16 June 2019/movieTickets.js
@@ -0,0 +1,30 @@
+function solve(input) {
+
+
+    let a1 = Number(input[0]);
+    let a2 = Number(input[1]);
+    let n = Number(input[2]);
+
+    for (let i = a1; i <= a2 - 1; i++) {
+
+        for (let j = 1; j <= n - 1; j++) {
+
+            for (let k = 1; k <= n / 2 - 1; k++) {
+
+                if (i % 2 !== 0 && ((i + j + k) % 2 !== 0)) {
+
+                    console.log(`${String.fromCharCode(i)}-${j}${k}${i}`);
+                } else {
+                    continue;
+                }
+            }
+        }
+
+    }
+
+}
+
+solve([
+    "86",
+    "88",
+    "4"]);
