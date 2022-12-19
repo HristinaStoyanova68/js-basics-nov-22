fb488f41bf42963b8ab6c6b319fd94424cd968db
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2022-12-20 00:02:12 +0200

    solved exercises assignment

--- Conditional Statements Advanced - More Exercises/multiplyBy2.js
+++ Conditional Statements Advanced - More Exercises/multiplyBy2.js
@@ -1,7 +1,16 @@
 function solve(input) {
 
-    
+    let index = 0;
+    let num = Number(input[index]);
+    index++;
 
+    while (num >= 0) {
+        let result = num * 2;
+        console.log(`Result: ${result.toFixed(2)}`);
+        num = Number(input[index]);
+        index++;
+    }
+    console.log("Negative number!");
 }
 
-solve(["12", "43.2144", "12.3", "543.23", "-20"]);
+solve(["23.43", "12.3245", "0", "65.23432", "23", "65", "-12"]);
