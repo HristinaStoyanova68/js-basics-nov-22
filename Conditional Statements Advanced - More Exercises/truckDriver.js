faede048a1b4f4250f83dc6dc45131851017851e
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2022-12-19 23:37:15 +0200

    solved exercises assignment

---
+++ Conditional Statements Advanced - More Exercises/truckDriver.js
@@ -0,0 +1,42 @@
+function solve(input) {
+
+    let seasson = input[0];
+    let kilometers = Number(input[1]);
+    let pricePerKm = 0;
+
+    switch (seasson) {
+        case "Spring":
+        case "Autumn":
+            if (kilometers <= 5000) {
+                pricePerKm = 0.75;
+            } else if (kilometers <= 10000) {
+                pricePerKm = 0.95;
+            } else {
+                pricePerKm = 1.45;
+            }
+            break;
+        case "Summer":
+            if (kilometers <= 5000) {
+                pricePerKm = 0.90;
+            } else if (kilometers <= 10000) {
+                pricePerKm = 1.10;
+            } else {
+                pricePerKm = 1.45;
+            }
+            break;
+        case "Winter":
+            if (kilometers <= 5000) {
+                pricePerKm = 1.05;
+            } else if (kilometers <= 10000) {
+                pricePerKm = 1.25;
+            } else {
+                pricePerKm = 1.45;
+            }
+            break;
+    }
+    let seassonPrice = kilometers * 4 * pricePerKm;
+    let total = seassonPrice * 0.90;
+    console.log(total.toFixed(2));
+}
+
+solve(["Winter", "4350"]);
